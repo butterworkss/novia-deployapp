82c886d86b7a5050f58618cb2760104c4b65bf54
noviatrp <142102648+butterworkss@users.noreply.github.com>
2023-11-07 12:40:21 +0700

    Add files via upload

---
+++ novia_profileappdeploy/script.js
@@ -0,0 +1,6 @@
+function toogleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toogle("open");
+    icon.classList.toogle("open");
+}
